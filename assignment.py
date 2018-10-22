3a696510b5934b4d924184ae4947785dfae0eecb
Yaroslav Govorunov <govorunov@gmail.com>
2018-10-22 17:32:21 +1100

    progress

--- assignment.py
+++ assignment.py
@@ -39,7 +39,7 @@ def read_data_from_list(data):
         raise DataError("Incorrect file format at line {}".format(line_n+2))
     if dimensions - 1 != line_n:  # dimensions include trailing '\n'
         raise DataError("Incorrect dimensions {}x{}. Square expected.".format(dimensions-1, line_n-1))
-    puzzle = [ln.lower().strip('\n') for ln in data[:line_n]]
+    puzzle = [bytearray(ln.lower().strip('\n')) for ln in data[:line_n]]
     words = [ln.lower().strip('\n') for ln in data[line_n+2:] if ln != "\n"]
 
     # make sure data contains letters only
@@ -65,6 +65,31 @@ def sanitize_data(puzzle, words):
         raise DataError("Data is looped, expected Tree or Forest")
 
 
+def search2D(puzzle, row, col, word):
+
+    # Directions - left, down, right, up
+    DIRS = [(1,0),(0,1),(-1,0),(0,-1)]
+
+    # Check starting point first
+    if puzzle[row][col] != word[0]:
+        return None
+
+    dim = len(puzzle)
+
+    # Search in multiple directions
+    for x, y in DIRS:
+        for i, ch in enumerate(word[1:]):
+            ri = row + i * x
+            ci = col + i * y
+            if ri < 0 or ri == dim or ci < 0 or ci == dim:
+                break
+            if ch != puzzle[ri][ci]:
+                break
+        else:
+            return ri, ci
+    return None
+
+
 def search_words(puzzle, words):
     """
     Searches for words in puzzle
@@ -73,12 +98,31 @@ def search_words(puzzle, words):
     :return:
     """
 
-
-def search2D(puzzle):
+    found = {}
+    dim = len(puzzle)
+    # Sort words by length
+    for word in sorted(words, key=len, reverse=True):
+        for row in xrange(0,dim):
+            for col in xrange(0,dim):
+                coord = search2D(puzzle, row, col, word)
+                if coord != None:
+                    x, y = coord
+                    found[word] = ((row,col),(x,y))
+                    # Uppercase found word to exclude it from further search
+                    for ri in range(row,x+1) if x > row else range(x,row+1):
+                        for ci in range(col, y+1) if y > col else range(y,col+1):
+                            puzzle[ri][ci] = chr(puzzle[ri][ci]).upper()
+    return found
 
 
 def print_results(words, found):
-    pass
+    for word in words:
+        if word in found:
+            (r, c), (x, y) = found[word]
+            print("{} ({},{}),({},{})".format(word.upper(),r,c,x,y))
+        else:
+            print("{} not found".format(word.upper()))
+
 
 def main(args, loglevel):
     """
@@ -90,24 +134,28 @@ def main(args, loglevel):
     logging.basicConfig(format="%(levelname)s: %(message)s", level=loglevel)
 
     # List of words to find
-    words = None
+    words = []
 
     # List of strings representing puzzle
-    puzzle = None
+    puzzle = []
 
     # List of coordinates tuples of found words
-    found = None
+    found = {}
 
     # Section that loads data
     try:
         # Open .pzl file
         with open(args.puzzle_file, 'r') as pzlfile:
             # Read data from pzl file
-            puzzle, words = read_data_from_list(pzlfile.readlines())
-            words.sort(key=len, reverse=True)
+            data = pzlfile.readlines()
+        # Parse data
+        puzzle, words = read_data_from_list(data)
 
         # Sanitize input data
-        dimensions = sanitize_data(puzzle, words)
+        #dimensions = sanitize_data(puzzle, words)
+
+        found = search_words(puzzle, words)
+        print_results(words, found)
 
     except (DataError, ValueError) as err:
         logging.error("Cannot read file {} - {}".format(args.puzzle_file, err))
